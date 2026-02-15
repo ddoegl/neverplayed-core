2a0a3b4196b7eaaaa4c53179cf39246796deb276
ddoegl <daniel@doegl.info>
2026-02-15 12:57:21 +0100

    templates per flow, fellow selector as component

--- serve.ts
+++ serve.ts
@@ -1,7 +1,14 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
-Deno.serve((req) => {
-  return serveDir(req, {
+Deno.serve(async (req) => {
+  const res = await serveDir(req, {
     fsRoot: "client",
   });
+  const enableCache = Deno.env.get("ENABLE_CACHE") === "true";
+  if (!enableCache) {
+    res.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.headers.set("Pragma", "no-cache");
+    res.headers.set("Expires", "0");
+  }
+  return res;
 });
